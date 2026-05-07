b1d5cf0bdabe831f0ee6bffd8486dc494234616f
thienne3110 <143318292+thienne3110@users.noreply.github.com>
2026-05-07 21:05:00 +0700

    Update BootReceiver.java

--- java/org/zauto/BootReceiver.java
+++ java/org/zauto/BootReceiver.java
@@ -3,12 +3,26 @@ package org.zauto;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            ZaloForegroundService.startService(context);
+        String action = intent.getAction();
+        
+        // Kiểm tra đúng tín hiệu máy đã khởi động xong
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action) || 
+            "android.intent.action.QUICKBOOT_POWERON".equals(action)) {
+            
+            Log.d("ZAuto", "Máy đã khởi động - Đang bật dịch vụ ZAuto VIP...");
+
+            try {
+                // Gọi dịch vụ chạy ngầm ngay lập tức
+                ZaloForegroundService.startService(context);
+            } catch (Exception e) {
+                Log.e("ZAuto", "Lỗi khởi động Boot: " + e.getMessage());
+            }
         }
     }
 }
